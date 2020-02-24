1b2070d8ca365fb5ffa22b5cbff9749c9ed6de44
Flatwaze <flatwaze@yandex.ru>
2020-02-24 19:21:00 +0300

    Урок 1: BugFix

--- UI/WebStore/Startup.cs
+++ UI/WebStore/Startup.cs
@@ -63,7 +63,7 @@ namespace WebStore
             );
 
 
-            services.AddScoped<IHttpContextAccessor, HttpContextAccessor>();
+            /*services.AddScoped<IHttpContextAccessor, HttpContextAccessor>();*/
             services.AddScoped<ICartService, CookieCartService>();
         }
 
