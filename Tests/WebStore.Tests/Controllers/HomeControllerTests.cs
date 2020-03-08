5f6249a84e4b220972f2fcb345d9252d668198e3
Flatwaze <flatwaze@yandex.ru>
2020-03-08 18:57:06 +0300

    Урок 5: HomeController Module Test

---
+++ Tests/WebStore.Tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WebStore.Controllers;
+using Assert = Xunit.Assert;
+
+namespace WebStore.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTests
+    {
+        [TestMethod]
+        public void Index_Return_View() {
+            var logger_mock = new Mock<ILogger<HomeController>>();
+            var controller = new HomeController(logger_mock.Object);
+            var result = controller.Index();
+            Assert.IsType<ViewResult>(result);
+        }
+
+    }
+}
