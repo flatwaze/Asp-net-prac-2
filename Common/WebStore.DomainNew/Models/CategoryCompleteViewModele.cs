3e9925ec7865bafe2b396ce447ddb8d0b6a0e3b0
Flatwaze <flatwaze@yandex.ru>
2020-03-11 22:31:15 +0300

    Урок 6: Внедерние. Баг не распознает кусок разр кода

---
+++ Common/WebStore.DomainNew/Models/CategoryCompleteViewModele.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WebStore.Models;
+
+namespace WebStore.DomainNew.Models
+{
+    public class CategoryCompleteViewModel
+    {
+        public IEnumerable<CategoryViewModel> Categories { get; set; }
+
+        public int? CurrentParentCategoryId { get; set; }
+
+        public int? CurrentCategoryId { get; set; }
+    }
+}
