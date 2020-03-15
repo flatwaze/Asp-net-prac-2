0cfc3a5625a7e61f078c97c1ff500ae69ddc615d
Flatwaze <flatwaze@yandex.ru>
2020-03-15 13:54:16 +0300

    Урок 8: Ajax Server

---
+++ Common/WebStore.DomainNew/Models/AjaxTestViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebStore.DomainNew.Models
+{
+    public class AjaxTestViewModel
+    {
+        public int Id { get; set; }
+        public string Message { get; set; }
+
+        public DateTime ServerTime { get; set; }
+    }
+}
---
+++ UI/WebStore/Controllers/AjaxTestController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.DomainNew.Models;
+
+namespace WebStore.Controllers
+{
+    public class AjaxTestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> GetJSON(int? id, string msg)
+        {
+            await Task.Delay(200);
+            return Json(new
+            {
+                Message = $"Response(id : {id ?? -1}) : {msg ?? "<null>"}",
+                ServerTime = DateTime.Now
+            });
+        }
+
+        public async Task<IActionResult> GetTestView(int? id, string msg)
+        {
+            await Task.Delay(2000);
+            return PartialView("Partial/_DataView", new AjaxTestViewModel
+            {
+                Id = id ?? -1,
+                Message = msg ?? "<null>",
+                ServerTime = DateTime.Now
+            });
+        }
+    }
+}
